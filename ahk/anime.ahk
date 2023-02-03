841e1557fc79fe27d487004dac71e4ab507d448e
kkard2 <kkard2.acct@gmail.com>
2023-02-03 20:13:40 +0100

    feat: bind Win+Z in browser to anime lists

---
+++ ahk/anime.ahk
@@ -0,0 +1,14 @@
+#IfWinActive ahk_exe firefox.exe
+#z::
+#IfWinActive ahk_exe msedge.exe
+#z::
+Send ^t
+Send ^l
+Send https://myanimelist.net/animelist/kkard2
+Send {Enter}
+Send ^t
+Send ^l
+Send https://anilist.co/user/kkard2/animelist
+Send {Enter}
+return
+
--- startup.ps1
+++ startup.ps1
@@ -3,4 +3,5 @@ $CurrentPath = Split-Path $MyInvocation.MyCommand.Path -Parent
 # ahk
 $AutoHotkeyPath = "C:\'Program Files'\AutoHotkey\v2\AutoHotkey64.exe"
 Invoke-Expression "$AutoHotkeyPath $CurrentPath\ahk\caps.ahk"
+Invoke-Expression "$AutoHotkeyPath $CurrentPath\ahk\anime.ahk"
 
