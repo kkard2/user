0eb1bf50aec933cb0acd5070eb7be28d75247d01
kkard2 <kkard2.acct@gmail.com>
2023-03-27 17:24:53 +0200

    refactor: move ahk powershell commands to separate script

---
+++ ahk/run.ps1
@@ -0,0 +1,5 @@
+$CurrentPath = Split-Path $MyInvocation.MyCommand.Path -Parent
+$AutoHotkeyPath = "C:\'Program Files'\AutoHotkey\v2\AutoHotkey64.exe"
+Invoke-Expression "$AutoHotkeyPath $CurrentPath\caps.ahk"
+Invoke-Expression "$AutoHotkeyPath $CurrentPath\anime.ahk"
+Invoke-Expression "$AutoHotkeyPath $CurrentPath\media.ahk"
--- startup.ps1
+++ startup.ps1
@@ -2,8 +2,4 @@ $CurrentPath = Split-Path $MyInvocation.MyCommand.Path -Parent
 Set-Location $CurrentPath
 git pull
 
-# ahk
-$AutoHotkeyPath = "C:\'Program Files'\AutoHotkey\v2\AutoHotkey64.exe"
-Invoke-Expression "$AutoHotkeyPath $CurrentPath\ahk\caps.ahk"
-Invoke-Expression "$AutoHotkeyPath $CurrentPath\ahk\anime.ahk"
-Invoke-Expression "$AutoHotkeyPath $CurrentPath\ahk\media.ahk"
+.\ahk\run.ps1
