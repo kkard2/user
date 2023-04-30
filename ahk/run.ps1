dcc708250cdece13d0ce62cdb37c14f4cdd3c17f
kkard2 <kkard2.acct@gmail.com>
2023-04-30 12:02:20 +0200

    fix: ahk scripts in elevated apps

--- ahk/run.ps1
+++ ahk/run.ps1
@@ -1,5 +1,5 @@
 $CurrentPath = Split-Path $MyInvocation.MyCommand.Path -Parent
-$AutoHotkeyPath = "C:\'Program Files'\AutoHotkey\v2\AutoHotkey64.exe"
+$AutoHotkeyPath = "C:\'Program Files'\AutoHotkey\v2\AutoHotkey64_UIA.exe"
 Invoke-Expression "$AutoHotkeyPath $CurrentPath\caps.ahk"
 Invoke-Expression "$AutoHotkeyPath $CurrentPath\anime.ahk"
 Invoke-Expression "$AutoHotkeyPath $CurrentPath\media.ahk"
